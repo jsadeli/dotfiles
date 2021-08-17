cb17710005fda03cd5a8e289a175e48591b6aa4f
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2021-08-17 20:03:08 +0700

    Added additional formulas, refine casks default.

--- brew.sh
+++ brew.sh
@@ -34,18 +34,20 @@ brew upgrade
 # ------------------------------------------------------------------------------
 
 formulas=(
-    git
-    # coreutils     # GNU core utilities (those that come with OS X are outdated)
-    # moreutils     # Collection of tools that nobody wrote when UNIX was young
-    # findutils     # Collection of GNU find, xargs, and locate
-    # htop          # Improved top (interactive process viewer)
-    glances         # Alternative to top/htop
-    mas             # Mac App Store command-line interface
-    python
-    ruby
-    node            # node.js | Platform built on V8 to build network applications
-    qpdf            # Tools for and transforming and inspecting PDF files
-    # pandoc
+    git             # Distributed revision control system.
+    # coreutils       # GNU core utilities (those that come with OS X are outdated).
+    # moreutils       # Collection of tools that nobody wrote when UNIX was young.
+    # findutils       # Collection of GNU find, xargs, and locate.
+    # htop            # Improved top (interactive process viewer).
+    glances         # Alternative to top/htop.
+    tree            # Display directories as trees.
+    peco            # Simplistic interactive filtering tool.
+    mas             # Mac App Store command-line interface.
+    python          # A programming language that lets you work quickly and integrate systems more effectively.
+    ruby            # Distributed revision control system.
+    node            # node.js | Platform built on V8 to build network applications.
+    qpdf            # Tools for and transforming and inspecting PDF files.
+    # pandoc          # Swiss-army knife of markup format conversion.
 )
 
 # Install packages
@@ -58,40 +60,40 @@ echo "Success! Homebrew formulas are installed."
 # ------------------------------------------------------------------------------
 
 casks=(
-    firefox
-    google-chrome
-    vivaldi
-    opera
-    microsoft-edge      # .pkg installer, requests password.
-    alfred
-    betterzip
-    default-folder-x
-    hyperdock
-    soundsource
-    moom
-    little-snitch
-    cloudflare-warp
-    hazel
-    bartender
-    spotify
-    # julia
-    docker
-    visual-studio-code
-    jetbrains-toolbox
-    imageoptim
-    sourcetree
-    handbrake
-    # authy
-    iina
-    vlc
-    downie
-    permute
-    microsoft-teams     # .pkg installer, requests password.
-    skype
-    # zoom              # .pkg installer, requests password.
-    microsoft-office    # .pkg installer, requests password.
-    libreoffice
-    soulver
+    firefox             # Web browser.
+    google-chrome       # Web browser.
+    vivaldi             # Web browser focusing on customization and control.
+    opera               # Web browser.
+    microsoft-edge      # [.pkg] Web browser.
+    alfred              # Application launcher and productivity software.
+    betterzip           # Utility to create and modify archives.
+    default-folder-x    # Utility to enhance the Open and Save dialogs in applications.
+    hyperdock           # Add Window previews to dock apps.
+    soundsource         # Sound and audio controller.
+    moom                # Utility to move and zoom windows—on one display.
+    little-snitch       # Host-based application firewall.
+    cloudflare-warp     # Free app that makes your Internet safer.
+    hazel               # Automated organization.
+    bartender           # Menu bar icon organizer.
+    spotify             # Music streaming service.
+    # julia               # Programming language for technical computing.
+    docker              # App to build and share containerized applications and microservices.
+    visual-studio-code  # Open-source code editor.
+    jetbrains-toolbox   # JetBrains tools manager.
+    imageoptim          # Tool to optimize images to a smaller size.
+    # sourcetree          # Graphical client for Git version control.
+    handbrake           # Open-source video transcoder.
+    # authy               # Two-factor authentication software.
+    iina                # Free and open-source media player.
+    vlc                 # Multimedia player.
+    downie              # Downloads videos from different websites.
+    permute             # Converts and edits video, audio or image files.
+    microsoft-teams     # [.pkg] Meet, chat, call, and collaborate in just one place.
+    # skype               # Video chat, voice call and instant messaging application.
+    # zoom              # [.pkg] Video communication and virtual meeting platform.
+    microsoft-office    # [.pkg] Microsoft Office suite.
+    # libreoffice         # Office suite.
+    soulver             # Notepad with a built-in calculator.
 )
 
 # Install cask packages
