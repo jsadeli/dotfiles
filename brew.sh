079dcd97834c41c21a1d4063de82f32d8b11e328
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2022-11-01 01:58:52 +0700

    updated default formulas and casks

--- brew.sh
+++ brew.sh
@@ -20,7 +20,7 @@
 # so that you don’t need sudo when you brew install.
 if ! command -v brew &> /dev/null; then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # (Optionally) Turn off brew's analytics https://docs.brew.sh/Analytics
@@ -45,7 +45,7 @@ formulas=(
     # tree            # Display directories as trees.
     # peco            # Simplistic interactive filtering tool.
     mas             # Mac App Store command-line interface.
-    # qpdf            # Tools for and transforming and inspecting PDF files.
+    qpdf            # Tools for and transforming and inspecting PDF files.
     # pandoc          # Swiss-army knife of markup format conversion.
     # gnupg           # GNU Pretty Good Privacy (PGP) package.
     # pyenv           # Python version management.
@@ -56,7 +56,7 @@ formulas=(
     # vue-client      # Standard tooling for Vue.js development.
     # angular-cli     # CLI tool for Angular.
     shellcheck      # Static analysis and lint tool, for (ba)sh scripts.
-    # cloc            # Statistics utility to count lines of code.
+    cloc            # Statistics utility to count lines of code.
 )
 
 # Install packages
@@ -69,44 +69,45 @@ echo "Success! Homebrew formulas are installed."
 # ------------------------------------------------------------------------------
 
 casks=(
-    # firefox             # Web browser.
+    firefox             # Web browser.
     google-chrome       # Web browser.
     # vivaldi             # Web browser focusing on customization and control.
     # opera               # Web browser.
     # microsoft-edge      # [.pkg] Web browser.
-    # alfred              # Application launcher and productivity software.
+    alfred              # Application launcher and productivity software.
     # path-finder         # File manager.
-    # betterzip           # Utility to create and modify archives.
-    # default-folder-x    # Utility to enhance the Open and Save dialogs in applications.
-    # hyperdock           # Add Window previews to dock apps.
-    # alt-tab             # Enable Windows-like alt-tab.
-    # soundsource         # Sound and audio controller.
-    # moom                # Utility to move and zoom windows—on one display.
-    # shottr              # Screenshot measurement and annotation tool.
-    # little-snitch       # Host-based application firewall.
-    # cloudflare-warp     # Free app that makes your Internet safer.
-    # hazel               # Automated organization.
-    # bartender           # Menu bar icon organizer.
-    # spotify             # Music streaming service.
+    betterzip           # Utility to create and modify archives.
+    default-folder-x    # Utility to enhance the Open and Save dialogs in applications.
+    hyperdock           # Add Window previews to dock apps.
+    alt-tab             # Enable Windows-like alt-tab.
+    soundsource         # Sound and audio controller.
+    moom                # Utility to move and zoom windows—on one display.
+    shottr              # Screenshot measurement and annotation tool.
+    little-snitch       # Host-based application firewall.
+    cloudflare-warp     # Free app that makes your Internet safer.
+    hazel               # Automated organization.
+    bartender           # Menu bar icon organizer.
+    spotify             # Music streaming service.
     # julia               # Programming language for technical computing.
     docker              # App to build and share containerized applications and microservices.
     visual-studio-code  # Open-source code editor, diff, merge.
-    # jetbrains-toolbox   # JetBrains tools manager.
-    # imageoptim          # Tool to optimize images to a smaller size.
+    jetbrains-toolbox   # JetBrains tools manager.
+    imageoptim          # Tool to optimize images to a smaller size.
     # handbrake           # Open-source video transcoder.
     # authy               # Two-factor authentication software.
-    # iina                # Free and open-source media player.
-    # vlc                 # Multimedia player.
-    # downie              # Downloads videos from different websites.
+    iina                # Free and open-source media player.
+    vlc                 # Multimedia player.
+    downie              # Downloads videos from different websites.
     # permute             # Converts and edits video, audio or image files.
     # microsoft-teams     # [.pkg] Meet, chat, call, and collaborate in just one place.
     # skype               # Video chat, voice call and instant messaging application.
     # zoom                # [.pkg] Video communication and virtual meeting platform.
-    # microsoft-office    # [.pkg] Microsoft Office suite.
-    # libreoffice         # Office suite.
+    microsoft-office    # [.pkg] Microsoft Office suite.
+    libreoffice         # Office suite.
     # soulver             # Notepad with a built-in calculator.
     gpg-suite-no-mail   # Tools to protect your files.
     quicklook-json        # QuickLook plugin for JSON files.
+    qlmarkdown            # QuickLook generator for Markdown files.
 )
 
 # Install cask packages
