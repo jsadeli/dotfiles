ecbdbf33d0dc083fb798e28bc1e00c53aa7245e8
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2023-06-05 04:25:13 +0700

    zsh and p10k symlinks

--- configs.sh
+++ configs.sh
@@ -12,3 +12,11 @@ ln -s "$HOME/dotfiles/configs/.npmrc" "$HOME/.npmrc"
 # .gitconfig
 rm -rf "$HOME/.gitconfig"
 ln -s "$HOME/dotfiles/configs/.gitconfig" "$HOME/.gitconfig"
+
+# .zshrc
+rm -rf "$HOME/.zshrc"
+ln -s "$HOME/dotfiles/configs/.zshrc" "$HOME/.zshrc"
+
+# .p10k.zsh
+rm -rf "$HOME/.p10k.zsh"
+ln -s "$HOME/dotfiles/configs/.p10k.zsh" "$HOME/.p10k.zsh"
