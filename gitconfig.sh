320d20157965c6973fe6a17d16a65edca9bcea6c
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2023-06-05 17:59:55 +0700

    support for vscode 3-way merge

--- gitconfig.sh
+++ gitconfig.sh
@@ -28,5 +28,5 @@ else
     git config --global diff.tool vscode
     git config --global difftool.vscode.cmd "code --wait --diff \$LOCAL \$REMOTE"
     git config --global merge.tool vscode
-    git config --global mergetool.vscode.cmd "code --wait \$MERGED"
+    git config --global mergetool.vscode.cmd "code --wait --merge \$REMOTE \$LOCAL \$BASE \$MERGED"
 fi
