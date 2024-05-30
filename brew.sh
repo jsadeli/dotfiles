0dd7e81a7f89284d53c39bbb250909bde1e4551d
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2024-05-30 15:42:19 +0700

    removed deprecated repository

--- brew.sh
+++ brew.sh
@@ -135,10 +135,6 @@ fonts_list=(
   font-jetbrains-mono-nerd-font
 )
 
-# Install fonts
-# Reference: https://gist.github.com/davidteren/898f2dcccd42d9f8680ec69a3a5d350e
-brew tap homebrew/cask-fonts
-
 for font in "${fonts_list[@]}"
 do
   brew install --cask "$font"
